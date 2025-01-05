4fae08d67a62a1e55206cabd3d584847157e08b2
Dominik Kowalczyk <dominik.kowalczyk17@gmail.com>
2025-01-05 01:04:47 +0100

    basic layout for messages and account settings

--- src/components/AccountDetails.tsx
+++ src/components/AccountDetails.tsx
@@ -3,12 +3,12 @@ import SettingsIcon from "@mui/icons-material/SettingsOutlined";
 
 export const AccountDetails = () => {
   return (
-    <div className="flex flex-col rounded-lg border-2 border-customGray m-4">
-      <div className="flex gap-10 p-4 justify-between h-[30%] border-2 border-b-customGray">
+    <div className="flex flex-col rounded-2xl border-2 border-customGray m-4">
+      <div className="flex gap-10 p-4 justify-between h-[30%] border-b-2 border-b-customGray">
         <span className="text-xl font-bold">Account Details</span>
         <SettingsIcon />
       </div>
-      <div className="flex gap-10 items-center p-4 bg-account h-[70%]">
+      <div className="flex gap-10 rounded-b-2xl items-center p-4 bg-account h-[70%]">
         <div className="h-16 block">
           <img className="h-full" src={Avatar} alt="avatar" />
         </div>
